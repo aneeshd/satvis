4590148573baa62d59b4a778f5dc323bc2dc57c3
Aneesh Dalvi <aneeshd@rogers.com>
2019-07-25 21:41:59 -0400

    Update get-constellation script; add O3b constellation

--- get-constellation.py
+++ get-constellation.py
@@ -1,15 +1,19 @@
-
+#!/usr/bin/env python3
 
 
 
 
 # https://celestrak.com/satcat/tle.php?CATNR=44057
 
-oneweb = [44057, 44058, 44059, 44060, 44061, 44062]
-starlink = range(44235, 44294+1)
-gx = [39476, 40384, 40882, 42698]
+constellations = {
+    'oneweb': [44057, 44058, 44059, 44060, 44061, 44062],
+    'starlink': range(44235, 44294+1),
+    'gx': [39476, 40384, 40882, 42698],
+    'o3b': [39188, 39189, 39190, 39191]+[40079, 40080, 40081, 40082]+[40348, 40349, 40350, 40351]+[43231, 43232, 43233, 43234]+[44112, 44113, 44114, 44115],
+}
 
 import urllib.request
+import sys
 
 def main(ids):
     for id in ids:
@@ -23,4 +27,5 @@ def main(ids):
         print(contents)
 
 if __name__=='__main__':
-    main(gx)
+    cname = sys.argv[1]
+    main(constellations[cname])
